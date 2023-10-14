2e14d9fb65b7275430a57483402457377f02b548
Felipe Printes <felipe.unilasalle@gmail.com>
2023-10-14 08:34:16 -0300

    post and get

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -4,27 +4,69 @@ import {useState, useEffect} from 'react'
 function TextField(){
 
     const [texto, setTexto] = useState('');
-    const [atividades, setAtividades] = useState([]); // Estado para armazenar a lista de atividades
+    // const [atividades, setAtividades] = useState([]); // Estado para armazenar a lista de atividades
+    const [atividade, setAtividade] = useState("")
     const [notes, setNotes] = useState([]);
+    const [msg, setMsg] = useState("")
     const limitText = 220
     
     const endpoint = 'http://localhost:5001/my_notes'
     // const endpoint = 'http://192.168.0.34:5001/my_notes'
     // const endpoint = 'https://jsonplaceholder.typicode.com/posts/1';
 
-    useEffect(()=>{
-        //Faz a requisição para api
-        fetch(endpoint)
-            .then(response => response.json())
-            .then(data => { setNotes(data)} )
-            .catch(error => { console.error('Erro ao buscar dados da api: ', error) })
+    const getActivity = async () => {
+        try{
+            const resposta = await fetch(endpoint)
+            if(resposta.ok){
+                const dadosRecebidos = await resposta.json()
+                setNotes(dadosRecebidos)
+            } else {
+                console.error("Erro na requisição")
+            }
+        } catch (erro) {
+            console.erro('Erro de requisição Get', erro)
+        }
+    }
+
+    // useEffect(()=>{
+    //     //Faz a requisição para api
+    //     fetch(endpoint)
+    //         .then(response => response.json())
+    //         .then(data => { setNotes(data)} )
+    //         .catch(error => { console.error('Erro ao buscar dados da api: ', error) })
+    // }, [])
+
+    useEffect(() => {
+        getActivity()
     }, [])
 
-    const activity = (evento) =>{
-        evento.preventDefault()
-        const conteudo = evento.target[0].value
-        if(conteudo !== ''){
-            setAtividades([...atividades,conteudo])
+    // const activity = (evento) =>{
+    //     evento.preventDefault()
+    //     const conteudo = evento.target[0].value
+    //     if(conteudo !== ''){
+    //         setAtividades([...atividades,conteudo])
+    //     }
+    // }
+
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            let res = await fetch('http://localhost:5001/new_note', {
+                method: 'POST',
+                body: JSON.stringify({
+                    content: atividade
+                })
+            })
+            let resJson = await res.json()
+            if (res.status===200){
+                setAtividade("")
+                setMsg("Atividade criada com sucesso")
+                getActivity();
+            }else {
+                setMsg("Erro ao cadastrar atividade")
+            }
+        } catch (err){
+            console.log(err)
         }
     }
 
@@ -33,12 +75,13 @@ function TextField(){
         
         if(e.target.value.length <=limitText){
             setTexto(e.target.value);
+            setAtividade(e.target.value);
         }
       };
 
     return (
         <div className="writeArea">
-            <form onSubmit={activity}>
+            <form onSubmit={handleSubmit}>
                 <label className="writeArea-label">
                     <input type="text" 
                     name="name" 
@@ -48,7 +91,7 @@ function TextField(){
                     placeholder='Guarde sua ideia com a gente'/>
                 </label>
                 <p>{texto.length}/{limitText}</p>
-                <input className='writeArea-button' value="Submit" type="submit"/>
+                <input className='writeArea-button' value='Add Nota' type="submit"/>
             </form>
             
             <div className='atividades'>
